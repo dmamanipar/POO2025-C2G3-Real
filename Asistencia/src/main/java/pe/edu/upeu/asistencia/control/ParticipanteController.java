5ae74dc07d36d01a300c350aa3bc6b022d39defc
dmamanipar <dmamanipar@unsa.edu.pe>
2025-09-09 09:19:03 -0500

    Reporte en Tableview

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
@@ -1,10 +1,18 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import pe.edu.upeu.asistencia.enums.Carrera;
 import pe.edu.upeu.asistencia.enums.TipoParticipante;
+import pe.edu.upeu.asistencia.modelo.Participante;
+import pe.edu.upeu.asistencia.servicio.ParticipanteServicioI;
 
 @Controller
 public class ParticipanteController {
@@ -15,10 +23,36 @@ public class ParticipanteController {
     @FXML
     private ComboBox<TipoParticipante> cbxTipoParticipante;
 
+    @FXML TableView<Participante> tableView;
+    ObservableList<Participante> participantes;
+    TableColumn<Participante, String> dniCol, nombreCol, apellidoCol, carreraCol, tipoPartCol;
+
+    @Autowired
+    ParticipanteServicioI ps;
     @FXML
     public void initialize() {
         cbxCarrera.getItems().setAll(Carrera.values());
         cbxTipoParticipante.getItems().setAll(TipoParticipante.values());
+        definirNombresColumnas();
+        listarParticipantes();
+    }
+    public void definirNombresColumnas(){
+        dniCol = new TableColumn("DNI");
+        nombreCol = new TableColumn("Nombre");
+        apellidoCol = new TableColumn("Apellido");
+        apellidoCol.setMinWidth(200);
+        carreraCol = new TableColumn("Carrera");
+        tipoPartCol = new TableColumn("Tipo Participante");
+        tipoPartCol.setMinWidth(160);
+        tableView.getColumns().addAll(dniCol, nombreCol, apellidoCol, carreraCol, tipoPartCol);
+    }
+
+    public void listarParticipantes(){
+        dniCol.setCellValueFactory(cellData ->
+                cellData.getValue().getDni());
+
+        participantes=FXCollections.observableArrayList(ps.findAll());
+        tableView.setItems(participantes);
     }
 
 }
